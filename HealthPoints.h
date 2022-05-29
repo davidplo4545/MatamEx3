2e55953c372c62b0cdd9e1b606208fcc66796e76
Yitzchak Grinboum <yitzchakg@campus.technion.ac.il>
2022-05-29 19:47:53 +0300

    HP docs

--- HealthPoints.h
+++ HealthPoints.h
@@ -7,37 +7,99 @@
 
 class HealthPoints{
 private:
+
     int m_currValue;
     int m_maxValue;
+
+     /*
+     * Compares between two healthPoints objects.
+     *
+     * @param hp1 - 1st HealthPoints object.
+     * @param hp2 - 2nd HealthPoints object.
+     * @return
+     *      A boolean value.
+    */
     friend bool operator==(const HealthPoints& hp1,const HealthPoints& hp2);
+
+     /*
+     * Compares between two healthPoints objects
+     *
+     * @param hp1 - 1st HealthPoints object.
+     * @param hp2 - 2nd HealthPoints object.
+     * @return
+     *      A boolean value.
+    */
     friend bool operator<(const HealthPoints& hp1,const HealthPoints& hp2);
+
+     /*
+     * Prints out hp.
+     *
+     * @param os - output channel.
+     * @param hp - hp value to print.
+     * @return
+     *      ostream.
+    */
     friend std::ostream& operator<<(std::ostream& os,const HealthPoints& hp);
     
 public:
+
+ /*
+     * C'tor of HealthPoints
+     *
+     * @param maxHp - The maximum hp for the hp object.
+     * @return
+     *      A new instance of HealthPoints.
+    */
     HealthPoints(int maxHp = 100);
+
+     /*
+     * Copy c'tor of HealthPoints class
+     *
+     * @param hp - hp Object to duplicate.
+     * @return
+     *      A new instance of HealthPoints.
+    */
     HealthPoints(const HealthPoints& hp);
+
+     /*
+     * D'tor of Card class.
+     * Here we are explicitly telling the compiler to use the default methods
+     * @return
+     *      no return.
+    */
    ~HealthPoints()=default;
+
 
 
     class InvalidArgument{};
 
+
+     /*
+     * Standard addition/substraction arithmetic operations. 
+     * @return
+     *      HealthPoints object.
+    */
     HealthPoints& operator=(const HealthPoints& hp);
     HealthPoints& operator+=(const int hpAdd);
     HealthPoints& operator-=(const int hpSub);
-
-    HealthPoints operator+(const int hpAdd);
-    HealthPoints operator-(const int hpAdd);
+    HealthPoints& operator+(const int hpAdd);
+    HealthPoints& operator-(const int hpAdd); 
 
 };
-
 
 HealthPoints operator+(const int hpAdd,const HealthPoints& hp);
 HealthPoints operator-(const int hpAdd,const HealthPoints& hp);
 
 
+
+     /*
+     * Standard addition/substraction arithmetic operations. 
+     * @return
+     *      boolean value.
+    */
 bool operator!=(const HealthPoints& hp1,const HealthPoints& hp2);
-bool operator>(const HealthPoints& hp1,const HealthPoints& hp2);
-bool operator<(const HealthPoints& hp1,const HealthPoints& hp2);
+bool operator> (const HealthPoints& hp1,const HealthPoints& hp2);
+bool operator< (const HealthPoints& hp1,const HealthPoints& hp2);
 bool operator>=(const HealthPoints& hp1,const HealthPoints& hp2);
 bool operator<=(const HealthPoints& hp1,const HealthPoints& hp2);
 
--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
+#include <cmath>
+
+#include <sstream>
+
+#include "HealthPoints.h"
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+HealthPoints hp_1(3);
+
+    std::cout << ( 3>hp_1 );
+
 }
